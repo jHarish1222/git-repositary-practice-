67385eaa1cb2cab0e57b5397277282a4550177c3
Raj Kiran Gade <raj.gade@relevancelab.com>
2017-01-19 16:15:02 +0530

    added user and group

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,12 +21,21 @@ if node['platform'] == "ubuntu"
 		action [:enable, :start]
 	end
 
+	group 'web_admin'
+	
+	user 'web_admin' do
+		group 'web_admin'
+		system true 
+		shell '/bin/bash'
+	end 
+
         template '/var/www/html/index.html' do
 		source 'index.html.erb'
 		mode '0644'
 		owner 'web_admin'
 		group 'web_admin'
 	end	
+	
 
 else
 	package 'httpd'
