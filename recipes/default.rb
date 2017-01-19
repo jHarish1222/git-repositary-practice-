3cea1300b83eb9dd6bbd703023fb6908148cbb2b
Raj Kiran Gade <raj.gade@relevancelab.com>
2017-01-19 13:19:05 +0530

    Add apahce2 configuration

---
+++ recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook:: learn_chef_apache2
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Update Package cache
+
+if node['platform'] == "ubuntu" 
+	apt_update 'Update apt cache daily' do
+		frequency 86_400
+		action :periodic
+	end
+
+	# Install Package apache 2
+	package 'apache2'
+
+	# Start Apache 2 Service
+	service 'apache2' do
+		supports :status => true
+		action [:enable, :start]
+	end
+
+	
+else
+	package 'httpd'
+	
+	service 'httpd' do
+		action [:enable, :start]
+	end
+	
+
+
+end
+
+
